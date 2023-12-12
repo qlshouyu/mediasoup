3c7e7a9e3a6cb12f1d2143f3feb2562fcd5d199f
lugao2 <lugao2@iflytek.com>
2023-12-12 20:43:21 +0800

    readme

--- worker/README.md
+++ worker/README.md
@@ -1,26 +1,42 @@
-```sh
-# 需要通过vs控制台进入
-cd worker
-# windows下编译 Debug版本
-meson setup --prefix "D:\workspaces\2.my_project\mediasoup\worker\out\Debug" --bindir "" --libdir "" --buildtype debug -Db_pie=true -Db_staticpic=true --reconfigure -Dms_log_trace=true -Dms_log_file_line=true --vsenv ".\out\Debug\build"
-# 生成FBS代码
-meson compile -C ".\out\Debug\build" flatbuffers-generator
-# 编译可执行程序
-meson compile -C ".\out\Debug\build" -j 2 mediasoup-worker
-# 编译静态库
-meson compile -C ".\out\Debug\build" -j 2 libmediasoup-worker
-```
-- Release模式编译
-```sh
 
-# 需要通过vs控制台进入
-cd worker
-# windows下编译 Release版本
-meson setup --prefix "D:\workspaces\2.my_project\mediasoup\worker\out\Release" --bindir "" --libdir "" --buildtype release -Db_ndebug=true -Db_pie=true -Db_staticpic=true --reconfigure -Dms_log_trace=true -Dms_log_file_line=true --vsenv ".\out\Release\build"
-# 生成FBS代码
-meson compile -C ".\out\Release\build" flatbuffers-generator
-# 编译可执行程序
-meson compile -C ".\out\Release\build" -j 2 mediasoup-worker
-# 编译静态库
-meson compile -C ".\out\Release\build" -j 2 libmediasoup-worker
-```
+## windows下编译Mediasoup
+- **准备环境**
+  - 安装python
+  - 安装 meson和ninja
+    ```sh
+    # 安装meson
+    pip install  meson
+    pip install ninja
+    ```
+- **生成vs工程**
+  ```sh
+  # Windows下生成visul studio 工程,如果出错清理之前的build目录
+  meson setup --prefix "D:\workspaces\2.my_project\mediasoup\worker\out\Debug" --bindir "bin" --libdir "lib" --buildtype debug --backend vs -Db_pie=true -Db_staticpic=true --reconfigure -Dms_log_trace=true -Dms_log_file_line=true --vsenv ".\out\Debug\build"
+  ```
+- **Debug版本编译**
+  ```sh
+  # 需要通过vs控制台进入
+  cd worker
+  # windows下编译 Debug版本,如果偶尔卡住直接enter执行
+  meson setup --prefix "D:\workspaces\2.my_project\mediasoup\worker\out\Debug" --bindir "bin" --libdir "lib" --buildtype debug -Db_pie=true -Db_staticpic=true --reconfigure -Dms_log_trace=true -Dms_log_file_line=true --vsenv ".\out\Debug\build"
+  # %cd%
+  # 生成FBS代码
+  meson compile -C ".\out\Debug\build" flatbuffers-generator
+  # 编译可执行程序
+  meson compile -C ".\out\Debug\build" -j 2 mediasoup-worker
+  # 编译静态库
+  meson compile -C ".\out\Debug\build" -j 2 libmediasoup-worker
+  ```
+- **Release版本编译**
+  ```sh
+  # 需要通过vs控制台进入
+  cd worker
+  # windows下编译 Release版本
+  meson setup --prefix "D:\workspaces\2.my_project\mediasoup\worker\out\Release" --bindir "" --libdir "" --buildtype release -Db_ndebug=true -Db_pie=true -Db_staticpic=true --reconfigure -Dms_log_trace=true -Dms_log_file_line=true --vsenv ".\out\Release\build"
+  # 生成FBS代码
+  meson compile -C ".\out\Release\build" flatbuffers-generator
+  # 编译可执行程序
+  meson compile -C ".\out\Release\build" -j 2 mediasoup-worker
+  # 编译静态库
+  meson compile -C ".\out\Release\build" -j 2 libmediasoup-worker
+  ```
--- worker/src/main.cpp
+++ worker/src/main.cpp
@@ -11,77 +11,77 @@ static constexpr int ProducerChannelFd{ 4 };
 static constexpr int PayloadConsumerChannelFd{ 5 };
 static constexpr int PayloadProducerChannelFd{ 6 };
 
-void readFnCall(uint8_t* msg, uint32_t msgLen, size_t ctx){
-	std::cout<<"readFnCall:"<<msg<<std::endl;
-}
+// void readFnCall(uint8_t* msg, uint32_t msgLen, size_t ctx){
+// 	std::cout<<"readFnCall:"<<msg<<std::endl;
+// }
 
-ChannelReadFreeFn readFn(
-  uint8_t** msg /* message */,
-  uint32_t* msgLen /* messageLen */,
-  size_t* msgCtx /* messageCtx */,
-  // This is `uv_async_t` handle that can be called later with `uv_async_send()`
-  // when there is more data to read.
-  const void*  handle/* handle */,
-  ChannelReadCtx ctx /* ctx */
-){
-	std::cout<<"readFn:"<<msg<<std::endl;
+// ChannelReadFreeFn readFn(
+//   uint8_t** msg /* message */,
+//   uint32_t* msgLen /* messageLen */,
+//   size_t* msgCtx /* messageCtx */,
+//   // This is `uv_async_t` handle that can be called later with `uv_async_send()`
+//   // when there is more data to read.
+//   const void*  handle/* handle */,
+//   ChannelReadCtx ctx /* ctx */
+// ){
+// 	std::cout<<"readFn:"<<msg<<std::endl;
 	
-	flatbuffers::FlatBufferBuilder builder;
+// 	flatbuffers::FlatBufferBuilder builder;
 
 
-	auto request= FBS::CreateRequest(builder,123456,
-		FBS::Request::Method::WORKER_CREATE_ROUTER,
-		builder.CreateString("handler_id1"),FBS::Request::Body::Worker_CreateRouterRequest);
+// 	auto request= FBS::CreateRequest(builder,123456,
+// 		FBS::Request::Method::WORKER_CREATE_ROUTER,
+// 		builder.CreateString("handler_id1"),FBS::Request::Body::Worker_CreateRouterRequest);
 
 
-	ChannelReadFreeFn fun=readFnCall;
-	std::cout<<"===== readFn ==="<<std::endl;
-	return nullptr;
-}
+// 	ChannelReadFreeFn fun=readFnCall;
+// 	std::cout<<"===== readFn ==="<<std::endl;
+// 	return nullptr;
+// }
 
-void writeFn(const uint8_t* msg /* message */, uint32_t msgLen/* messageLen */, ChannelWriteCtx ctx/* ctx */){
-	std::cout<<"writefn:"<<msg<<std::endl;
-}
+// void writeFn(const uint8_t* msg /* message */, uint32_t msgLen/* messageLen */, ChannelWriteCtx ctx/* ctx */){
+// 	std::cout<<"writefn:"<<msg<<std::endl;
+// }
 
 int main(int argc, char* argv[])
 {
 	// Ensure we are called by our Node library.
-	// if (!std::getenv("MEDIASOUP_VERSION"))
-	// {
-	// 	MS_ERROR_STD("you don't seem to be my real father!");
+	if (!std::getenv("MEDIASOUP_VERSION"))
+	{
+		MS_ERROR_STD("you don't seem to be my real father!");
 
-	// 	// 41 is a custom exit code to notify about "missing MEDIASOUP_VERSION" env.
-	// 	std::_Exit(41);
-	// }
+		// 41 is a custom exit code to notify about "missing MEDIASOUP_VERSION" env.
+		std::_Exit(41);
+	}
 
-	// const std::string version = std::getenv("MEDIASOUP_VERSION");
+	const std::string version = std::getenv("MEDIASOUP_VERSION");
 
-	// auto statusCode = mediasoup_worker_run(
-	//   argc,
-	//   argv,
-	//   "3.0",
-	//   ConsumerChannelFd,
-	//   ProducerChannelFd,
-	//   PayloadConsumerChannelFd,
-	//   PayloadProducerChannelFd,
-	//   nullptr,
-	//   nullptr,
-	//   nullptr,
-	//   nullptr);
-	ChannelReadCtx rCtx=nullptr;
-	ChannelWriteCtx wCtx=nullptr;
 	auto statusCode = mediasoup_worker_run(
 	  argc,
 	  argv,
 	  "3.0",
-	  0,
-	  0,
-	  0,
-	  0,
-	  readFn,
-	  rCtx,
-	  writeFn,
-	  wCtx);
+	  ConsumerChannelFd,
+	  ProducerChannelFd,
+	  PayloadConsumerChannelFd,
+	  PayloadProducerChannelFd,
+	  nullptr,
+	  nullptr,
+	  nullptr,
+	  nullptr);
+	// ChannelReadCtx rCtx=nullptr;
+	// ChannelWriteCtx wCtx=nullptr;
+	// auto statusCode = mediasoup_worker_run(
+	//   argc,
+	//   argv,
+	//   "3.0",
+	//   0,
+	//   0,
+	//   0,
+	//   0,
+	//   readFn,
+	//   rCtx,
+	//   writeFn,
+	//   wCtx);
 
 	std::_Exit(statusCode);
 }
